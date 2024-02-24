d26f5ccb66caf772c3324f6bea77f4c95754f549
Yug Jadvani <110123287+yugjadvani-dev@users.noreply.github.com>
2024-02-24 19:44:49 +0530

    Refactor SingleMessage component and remove unused dependency

--- src/components/SingleMessage.tsx
+++ src/components/SingleMessage.tsx
@@ -1,52 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import CountryPicker, {Country} from 'react-native-country-picker-modal';
 import {COLORS, SPACING} from '../theme/theme';
 import {TouchableOpacity} from 'react-native';
 
 const SingleMessage = () => {
-  const [selectedCountry, setSelectedCountry] = React.useState<
-    Country | null | undefined
-  >(null);
-  console.log(
-    '🚀 ~ file: SingleMessage.tsx:7 ~ selectedCountry 😀👏:',
-    selectedCountry,
-  );
-
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.countryCode}>
-        <CountryPicker
-          withFlag
-          withCallingCode
-          withEmoji
-          countryCode="US"
-          onSelect={country => setSelectedCountry(country)}
-          visible
-          //   countryCode={selectedCountry?.cca2}
-        />
+        <Text>Hello</Text>
       </TouchableOpacity>
-      {/* <Text>
-        {selectedCountry
-          ? `${selectedCountry?.flag} ${selectedCountry?.name} +${selectedCountry?.callingCode}`
-          : 'Select a country'}
-      </Text> */}
-      {/* {selectedCountry && (
-        <View>
-          <CountryPicker
-            withFlagButton={false}
-            countryCode={selectedCountry.cca2}
-            withFlag
-            withEmoji={false}
-            withFilter={false}
-          />
-          <Text>
-            {selectedCountry
-              ? `${selectedCountry?.name} +${selectedCountry?.callingCode}`
-              : 'Select a country'}
-          </Text>
-        </View>
-      )} */}
     </View>
   );
 };
